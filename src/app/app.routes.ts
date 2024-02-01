37becbf6e47072c2b70cdaec97a0cc276d47c1c4
agustindiaz59 <matutias10000@gmail.com>
2024-02-01 14:04:39 -0300

    segunda version estable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PreviewComponent } from './components/preview/preview.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: HomeComponent},
+    {path: 'producto/:i', component: PreviewComponent},
     {path: '**', redirectTo: 'inicio'}
 ];
---
+++ src/app/components/preview/preview.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigatorComponent } from '../navigator/navigator.component';
+import { articulo } from '../../models/articulo.interface';
+import { ArticlesService } from '../../services/articles.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-preview',
+  standalone: true,
+  imports: [CommonModule,NavigatorComponent, HttpClientModule, FooterComponent],
+  providers:[ArticlesService],
+  templateUrl: './preview.component.html',
+  styleUrl: './preview.component.css'
+})
+export class PreviewComponent implements OnInit{
+  protected articulo : articulo | undefined;
+
+  constructor(private aService: ArticlesService, private route: ActivatedRoute){
+  }
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) =>{
+        this.aService.getArticulos().subscribe(
+          (data: articulo[]) => {
+            this.articulo = data[Number(params['i'])]
+          }
+        )
+      }
+    )
+  }
+}
